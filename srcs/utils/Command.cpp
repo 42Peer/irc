672efe6ea3b1a7b94599c71c3d1a7b3995a2def6
Jiho Kang <jihkang@c2r4s8.42seoul.kr>
2023-04-18 17:48:43 +0900

    fix #23
    nick name loop fix

--- srcs/utils/Command.cpp
+++ srcs/utils/Command.cpp
@@ -41,6 +41,7 @@ void Notice::run(int fd, std::vector<std::string> args) {
 void Join::run(int fd, std::vector<std::string> args) {
 	std::string nick_name(this->_handler.getServer().getUserName(fd));
 	std::string buf("");
+
 	for (size_t i = 0; i < args.size(); ++i) {
 		if (args[i][0] != '#') {
 			this->_handler.getServer().setFdMessage(fd, ERR476);
