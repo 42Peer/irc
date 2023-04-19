96d739488bf69a1f85697ec448cb4a19a312ca6a
Soksurim <briochefavori@gmail.com>
2023-04-19 15:38:40 +0900

    [FIX] NICK error -  최신 dev 반영 - Irssi 중복 닉네임 처리가 동작 안함 - 닉네임 뒤에 _ 1 2 3..... 붙는거  닉 변경시 클라이언트 닉네임 표시 공간에 반영 안됨 - 로비에서 닉네임 변경 뒤 채팅방들어가면 원래 닉까지 2개가 들어가짐

--- srcs/utils/Command.cpp
+++ srcs/utils/Command.cpp
@@ -85,51 +85,59 @@ void Join::run(int fd, std::vector<std::string> args) {
 void Nick::run(int fd, std::vector<std::string> args) {
 	std::string buf("");
 	std::string new_nick = args[0];
+	std::string current_nick(this->_handler.getServer().getUserName(fd));
+	if (current_nick == "")
+	{
+			std::cout << "new client tmp nick is *" << "\n";
+		current_nick = "*";
+	}
+
 	if (!isValidName(new_nick)) {
-		buf.append("432 " + this->_handler.getServer().getUserName(fd) + " " + new_nick + " :Erroneus nickname\r\n");
+		buf.append("432 " + current_nick + " " + new_nick + " :Erroneus nickname\r\n");
 		this->_handler.getServer().setFdMessage(fd, ERR432);
 		return;
 	}
-	else if (this->_handler.getServer().g_db.getUserTable().isExist(new_nick)) {
+	if (this->_handler.getServer().g_db.getUserTable().isExist(new_nick)) {
 		buf.append("433 ");
-		if (this->_handler.getServer().getUserName(fd) == "") {
-			buf.append(" * ");
-		} else {
-			buf.append(this->_handler.getServer().getUserName(fd) + " ");
-		}
+		buf.append(current_nick);
+		buf.append(" ");
 		buf.append(new_nick);
 		buf.append(" :Nickname is already in use\r\n");
 		this->_handler.getServer().setFdMessage(fd, buf);
 		return;
-	} else {
+	} 
+
+	s_user_info old_user_info = this->_handler.getServer().g_db.getUserTable().getUser(current_nick);
+	// Pass 통과 했으면
+	if (this->_handler.getServer().getFdFlagsStatus(fd, 0))
+	{
+		// 가입 전이면 새 client 구조체 할당
+		if (!this->_handler.getServer().getFdFlagsStatus(fd, 1))
+		{
+			std::cout << "new client info created" << "\n";
+			struct s_user_info new_client;
+			new_client.fd = fd;
+			new_client.nick = new_nick;
+			new_client.name = "*";
+			new_client.real = "*";
+			this->_handler.getServer().g_db.getUserTable().addUser(new_client);
+			old_user_info = this->_handler.getServer().g_db.getUserTable().getUser(new_nick);
+			this->_handler.getServer().setFdFlagsOn(fd, 1);
+		}
+
 		struct s_user_info new_user_info;
 		new_user_info.nick = new_nick;
-		if (!this->_handler.getServer().getFdFlagsStatus(fd, 1)) {
-			new_user_info.fd = fd;
-			this->_handler.getServer().setFdFlagsOn(fd, 1);
-			this->_handler.getServer().g_db.getUserTable().addUser(new_user_info);
-			// if (this->_handler.getServer().getFdFlagsInitStatus(fd))
-		}
-		else {
-			std::string old_name = this->_handler.getServer().getUserName(fd);
-			s_user_info old_user_info = this->_handler.getServer().g_db.getUserTable().getUser(old_name);
-			new_user_info.name = old_user_info.name;
-			new_user_info.fd = old_user_info.fd;
+		new_user_info.name = old_user_info.name;
+		new_user_info.real = old_user_info.real;
+		new_user_info.fd = old_user_info.fd;
+		new_user_info.channel_list = old_user_info.channel_list;
 
-			this->_handler.getServer().g_db.updateUser(old_user_info, new_user_info);
+		this->_handler.getServer().g_db.updateUser(old_user_info, new_user_info);
 
-			buf.append(":" + this->_handler.getServer().getUserName(fd) + " NICK :" + new_nick + "\r\n");
-//			this->_handler.getServer().setFdMessage(fd, buf);
-
-//			나 포함 동일 채널의 사람들에게 닉네임 변경 사실 알려주기
-			std::string current_channel = this->_handler.getServer().g_db.getUserTable().getChannelList(new_user_info);
-			std::vector<std::string> users_in_current_channel = this->_handler.getServer().g_db.getCorrectChannel(current_channel).getUserList();
-			int receiver(0);
-			for (size_t i = 0; i < users_in_current_channel.size(); ++i)
-			 	receiver = this->_handler.getServer().g_db.getUserTable().getUser(users_in_current_channel[i]).fd;
-			 	this->_handler.getServer().setFdMessage(receiver, buf);
-		}
+		buf.append(":" + this->_handler.getServer().getUserName(fd) + " NICK " + new_nick + "\r\n");
+		this->_handler.getServer().setFdMessage(fd, buf);
 	}
+
 	this->_handler.getServer().setMapData(fd, new_nick);
 	if (this->_handler.getServer().checkGreetingMessage(fd)){
 		this->_handler.getServer().setFdMessage(fd, CODEGREET + new_nick +  MSGGREETING);
